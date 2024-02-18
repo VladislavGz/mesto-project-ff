97e69d0015bbb093855c6410affb2289d348ef9b
Vladislav Guzenkov <vladislav.srg@yandex.ru>
2024-02-18 21:16:22 +0300

    Добавлены функции открытия попапов и соответствующие слушатели

--- src/index.js
+++ src/index.js
@@ -2,36 +2,76 @@ import './pages/index.css';
 import initialCards from "./scripts/cards";
 
 
-// @todo: Темплейт карточки
+//Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content.querySelector('.card');
 
-// @todo: DOM узлы
+//DOM узлы
 const cardList = document.querySelector('.places__list');
+const popupEdit = document.querySelector('.popup_type_edit');
+const popupNewCard = document.querySelector('.popup_type_new-card');
+const popupImage = document.querySelector('.popup_type_image');
+const editButton = document.querySelector('.profile__edit-button');
+const addCardButton = document.querySelector('.profile__add-button');
 
-// @todo: Функция создания карточки
+//Функция создания карточки
 function createCard (dataCard, delCallback) {
     const card = cardTemplate.cloneNode(true);
 
     const title = card.querySelector('.card__title');
     const img = card.querySelector('.card__image');
     const deleteBtn = card.querySelector('.card__delete-button');
-    
+
     title.textContent = dataCard.name;
 
     img.setAttribute('src', dataCard.link);
     img.setAttribute('alt', `Фото: ${dataCard.name}`);
 
     deleteBtn.addEventListener('click', delCallback);
+    img.addEventListener('click', () => {
+        openImage({
+            src: dataCard.link,
+            alt: `Фото: ${dataCard.name}`,
+            txt: dataCard.name
+        });
+    });
 
     return card;
 };
 
-// @todo: Функция удаления карточки
+//Функция удаления карточки
 function deleteCard (evt) {
     evt.target.closest('.card').remove();
 }
 
-// @todo: Вывести карточки на страницу
+//функция открытия попапа
+function openPopup (popup) {
+    popup.classList.add('popup_is-opened');
+}
+
+//функция открытия изображения
+function openImage (imgData) {
+    const img = popupImage.querySelector('.popup__image');
+    const txt = popupImage.querySelector('.popup__caption');
+    img.setAttribute('src', imgData.src);
+    img.setAttribute('alt', imgData.alt);
+    txt.textContent = imgData.txt;
+    openPopup(popupImage);
+}
+
+//открытие окна редактирования профиля
+editButton.addEventListener('click', () => {
+    openPopup(popupEdit);
+});
+
+//открытие окна добавления новой карточки
+addCardButton.addEventListener('click', () => {
+    openPopup(popupNewCard);
+});
+
+
+
+
+//Вывести карточки на страницу
 initialCards.forEach(elem => {
     cardList.append(createCard(elem, deleteCard));
 });
