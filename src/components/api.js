845d8f71638ae05223ae2b65543817b97021a22b
Vladislav Guzenkov <vladislav.srg@yandex.ru>
2024-02-28 20:45:48 +0300

    Добавлен объект конфигурации запроса к серверу

---
+++ src/components/api.js
@@ -0,0 +1,8 @@
+const config = {
+    baseUrl: 'https://nomoreparties.co/v1/',
+    cohortId: 'wff-cohort-8',
+    headers: {
+        authorization: 'e90a88de-4d84-48ee-a98b-66385728ff23',
+        'Content-Type': 'application/json'
+    }
+};
