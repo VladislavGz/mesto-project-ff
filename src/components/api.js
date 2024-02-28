0f9753564f3af9ea6e774c315fd38a650215b7b8
Vladislav Guzenkov <vladislav.srg@yandex.ru>
2024-02-28 20:58:53 +0300

    Добавлена функция запроса данных о пользователе

--- src/components/api.js
+++ src/components/api.js
@@ -1,3 +1,4 @@
+//объект конфигурации запроса
 const config = {
     baseUrl: 'https://nomoreparties.co/v1/',
     cohortId: 'wff-cohort-8',
@@ -5,4 +6,19 @@ const config = {
         authorization: 'e90a88de-4d84-48ee-a98b-66385728ff23',
         'Content-Type': 'application/json'
     }
+};
+
+//функция запроса данных о пользователе
+function getUser () {
+    return fetch(`${config.baseUrl}${config.cohortId}/users/me`, {
+        headers: config.headers
+    })
+        .then(res => {
+            if (res.ok) return res.json();
+            return Promise.reject(`Ошибка: ${res.status}`);
+        });
+}
+
+export {
+    getUser
 };
