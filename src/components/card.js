b9662ef7cfcd51580d3b15fa1629e18ef3f82f01
Vladislav Guzenkov <vladislav.srg@yandex.ru>
2024-02-29 00:04:19 +0300

    Добавлена обработка счетчика лайков

--- src/components/card.js
+++ src/components/card.js
@@ -6,8 +6,10 @@ function createCard (cardTemplate, dataCard, delCallback, likeCard, openImage) {
     const img = card.querySelector('.card__image');
     const deleteBtn = card.querySelector('.card__delete-button');
     const likeBtn = card.querySelector('.card__like-button');
+    const likeCounter = card.querySelector('.card__like-counter');
 
     title.textContent = dataCard.name;
+    likeCounter.textContent = dataCard.likes;
 
     img.setAttribute('src', dataCard.link);
     img.setAttribute('alt', `Фото: ${dataCard.name}`);
