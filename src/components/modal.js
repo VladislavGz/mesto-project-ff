4702c09de19a8122e81b969a782b3b1115623e9b
Vladislav Guzenkov <vladislav.srg@yandex.ru>
2024-03-01 15:35:46 +0300

    Имена функций изменены: теперь они начинаются с глагола

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,26 +1,26 @@
 //функция открытия попапа
 function openPopup (popup) {
     popup.classList.add('popup_is-opened');
-    popup.addEventListener('click', popupClickHandler);
-    document.addEventListener('keydown', popupEscapeHandler);
+    popup.addEventListener('click', handlePopupClick);
+    document.addEventListener('keydown', handlePopupEscape);
 }
 
 //функция закрытия попапа
 function closePopup (popup) {
     popup.classList.remove('popup_is-opened');
-    popup.removeEventListener('click', popupClickHandler);
-    document.removeEventListener('keydown', popupEscapeHandler);
+    popup.removeEventListener('click', handlePopupClick);
+    document.removeEventListener('keydown', handlePopupEscape);
 }
 
 //функция обработки события клика по попапу
-function popupClickHandler (evt) {
+function handlePopupClick (evt) {
     if (evt.target.classList.contains('popup') || evt.target.classList.contains('popup__close')) {
         closePopup(evt.currentTarget);
     }
 }
 
 //функция обработки события нажатия Escape
-function popupEscapeHandler (evt) {
+function handlePopupEscape (evt) {
     if (evt.key === 'Escape') {
         const popup = document.querySelector('.popup_is-opened');
         closePopup(popup);
